4b72f77f668608f9c7fc30da8ce3561018de9746
b-luzi <brendon.luzi@outlook.it>
2022-11-18 21:46:50 +0100

    Hello

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,48 +1,29 @@
 import React from "react";
 import "./about.css";
-import { useTranslation } from "react-i18next";
 import Ima from "../../images/assyzdyk.jpg";
 import ImaPic from "../../images/istockphoto-1300845620-170667a.jpg";
 
 const About = () => {
-  const { t } = useTranslation();
   return (
     <div className="container about">
       <div className="myself">
         <div className="slotE">
-          <img src={Ima} alt="PhotoImg" id="PhotoImg"/>
-          <p>Sandra baloc</p>
-          <p>125</p>
+          <div className="evalBox">
+            <p class="level">lv.3</p>
+            <img src={Ima} alt="PhotoImg" id="evalPic"/>
+            <p>Bob</p>
+          </div>
+          <div className="evalBox">
+            <p class="level">lv.1</p>
+            <img src={ImaPic} alt="PhotoImg" id="evaluatedPic"/>
+            <p>Pipi</p>
+          </div>
+          <p class="score">125</p>
           <p>Get next line</p>
-          <img src={ImaPic} alt="PhotoImg" id="PhotoImg"/>
-        </div>
-        <div className="slotE">
-          <img src={Ima} alt="PhotoImg" id="PhotoImg"/>
-          <p>Sandra baloc</p>
-          <p>100</p>
-          <p>Printf</p>
-          <img src={ImaPic} alt="PhotoImg" id="PhotoImg"/>
-        </div>
-        <div className="slotE">
-          <img src={Ima} alt="PhotoImg" id="PhotoImg"/>
-          <p>Sandra baloc</p>
-          <p>110</p>
-          <p>Born2beroot</p>
-          <img src={ImaPic} alt="PhotoImg" id="PhotoImg"/>
-        </div>
-        <div className="slotE">
-          <img src={Ima} alt="PhotoImg" id="PhotoImg"/>
-          <p>Sandra baloc</p>
-          <p>100</p>
-          <p>Libft</p>
-          <img src={ImaPic} alt="PhotoImg" id="PhotoImg"/>
-        </div>
-        <div className="slotE">
-          <img src={Ima} alt="PhotoImg" id="PhotoImg"/>
-          <p>Sandra baloc</p>
-          <p>125</p>
-          <p>Get next line</p>
-          <img src={ImaPic} alt="PhotoImg" id="PhotoImg"/>
+          <div class="subBtn">
+            <button>Subscribe</button>
+            <p>3 / 9</p>
+          </div>
         </div>
       </div>
     </div>
